933568c807f34c6b4090959860a6dea6d0d9df2b
Derrik Wolthuis <derrik@trendwerk.nl>
2016-10-12 21:44:49 +0200

    Adding some animation

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,37 @@
-$('body').addClass('test');
+jQuery(($) => {
+  const $body = $('body');
+  const $intro = $('.intro');
+  const $window = $(window);
+
+  let introHeight;
+  let isFixed;
+
+  function calculateHeight() {
+    introHeight = $intro.outerHeight();
+  }
+
+  function scrolling() {
+    const scrollTop = $window.scrollTop();
+
+    if (scrollTop > introHeight) {
+      if (! isFixed) {
+        $body.addClass('show-nav');
+        isFixed = true;
+      }
+    } else {
+      if (isFixed) {
+        $body.removeClass('show-nav');
+        isFixed = false;
+      }
+    }
+
+    requestAnimationFrame(scrolling);
+  }
+
+  $window.resize(() => {
+    calculateHeight();
+  });
+
+  calculateHeight();
+  requestAnimationFrame(scrolling);
+});
